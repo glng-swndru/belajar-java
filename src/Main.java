029c65c2698c5a9e27d2f8711f3250deb82b4afe
Gilang <gilangswandaru27@gmail.com>
2025-05-26 06:44:32 +0800

    belajar java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("I like Pizza");
+        System.out.println("I like you too!");
+        System.out.println("Nice Try");
+
+        int age = 12;
+        System.out.println(age);
+    }
+}
